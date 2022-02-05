8510a95651d9b2d8a6f05c6f3760f1232d9d9e23
Kim Honrada <kimhonrada21@gmail.com>
2022-02-04 20:33:03 -0500

    Add dynamic command fetching

--- commands/yoo.js
+++ commands/yoo.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("bing")
-    .setDescription("bingbingbing"),
+    .setName("yooo")
+    .setDescription("yoooooosadasdaso"),
   async execute(interaction) {
-    await interaction.reply(`bong! what's up ariana grande?????`);
+    await interaction.reply(`this is a new command bb`);
   },
 };
