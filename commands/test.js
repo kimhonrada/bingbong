7c476784982b9306da4b7e8464d74190893b3bbb
Kim Honrada <kimhonrada21@gmail.com>
2022-02-10 14:58:32 -0500

    Add ready.js and interactionCreate.js in events folder

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,9 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("test").setDescription("test ommand"),
+  data: new SlashCommandBuilder()
+    .setName("test")
+    .setDescription("test command"),
   async execute(interaction) {
     await interaction.reply(`this is another test command`);
   },
